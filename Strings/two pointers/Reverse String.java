4c4f4ac725f58458efaeae076575a6c06c9b8a79
sadikahamed <psadikahamed27@gmail.com>
2026-07-31 16:59:54 +0530

    Implement reverse string function using two pointers

---
+++ Strings/two pointers/Reverse String.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void reverseString(char [] s){
+        int i=0;
+        int j=s.length-1;
+        while(i<j){
+            char temp=s[j];
+            s[j]=s[i];
+            s[i]=temp;
+            i++;
+            j--;
+        }
+    }
+    public static void main (String args[]){
+        char[] s ={'h','e','l','l','o'};
+        reverseString(s);
+        System.out.println(s);
+    }
+}
